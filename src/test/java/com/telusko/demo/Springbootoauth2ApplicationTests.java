f6c0da412ef7daee0bf3ee61f85a0b592c24e866
subhajitsaha999 <61118603+subhajitsaha999@users.noreply.github.com>
2021-02-09 21:18:52 +0530

    Update Springbootoauth2ApplicationTests.java

--- src/test/java/com/telusko/demo/Springbootoauth2ApplicationTests.java
+++ src/test/java/com/telusko/demo/Springbootoauth2ApplicationTests.java
@@ -2,7 +2,7 @@ package com.telusko.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+//This is test only
 @SpringBootTest
 class Springbootoauth2ApplicationTests {
 
